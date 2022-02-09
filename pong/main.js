e756b00c6357f8f477602d9aad27092f0b7548bd
Vicente Velasco <velasco.josevicente@gmail.com>
2022-02-08 20:04:34 -0500

    Se encuentra que las funciones se pueden comentar con documentacion usando /** justo encima de su declaracion

--- pong/main.js
+++ pong/main.js
@@ -1,4 +1,4 @@
-(function(){
+(function(){//MODELO
     //Funcion anonima que se ejecuta a si misma. Closure
     /* NOTA: self tiene inicialmente asignado el objeto Window. 
     Por tanto, las funciones son asignadas a este objeto y pueden usarse
@@ -21,7 +21,22 @@
     }
 })();
 
-(function(){
+(function (){
+    /**
+     * 
+    * @param {number} x La posicion en x de la barra
+     * @param {number} y La posicion en y de la barra
+     * @param {number} width El ancho de la barra
+     * @param {number} height  La altura de la barra
+     * @param {object} board Objeto tipo Board
+     */
+    self.Bar = function (x,y,width, height, board){
+        this.
+
+    }
+})();
+
+(function(){//VISTA
     self.BoardView = function(canvas, board){
         this.canvas = canvas
         //Las dimensiones del canvas son modificadas por las dimensiones del Board
@@ -39,9 +54,12 @@ parte del script, siempre que se esté en la misma ventana
 */
 window.addEventListener('load',main)//Evento que ejecuta la funcion main al cargar la pagina
 
-function main(){
+function main(){//CONTROLADOR
     //Funcion que ejecuta todos los elementos
     let board = new Board(800,400)
     let canvas = document.getElementById('canvas')
+    /**
+     * El controlador (funcion main()) le pasa a la vista (funcion BoardView()) el modelo (funcion Board()
+     */
     let board_view = new BoardView(canvas, board)
 }
